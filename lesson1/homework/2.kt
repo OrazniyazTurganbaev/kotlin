c34a42db1f14bd718beddba3d42e2e81a53269c4
OrazniyazTurganbaev <113829606+OrazniyazTurganbaev@users.noreply.github.com>
2022-09-18 15:44:07 +0500

    Update 2.kt

---
+++ lesson1/homework/2.kt
@@ -0,0 +1,18 @@
+/*
+2.	Kvadrattin’ tarepi a berilgen. Onin’ maydani aniqlansin. S=a*a
+*/
+
+import java.util.Scanner
+
+fun main()
+{
+	val scanner : Scanner = Scanner(System.`in`)
+
+	print("Kvadrattin’ tarepin kiritin' a=")
+	var a = scanner.nextInt()
+    	println()
+	
+	var S = a*a
+
+	print("Kvadrattin’ maydani S=$S")
+}
