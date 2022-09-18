991bf3dd6386455b4835cfd4fc3fada104efa556
OrazniyazTurganbaev <113829606+OrazniyazTurganbaev@users.noreply.github.com>
2022-09-18 15:36:40 +0500

    homework1

---
+++ lesson1/homework/1.kt
@@ -0,0 +1,17 @@
+/*
+1.	Kvadrattin’ tarepi a berilgen. Onin’ perimetri aniqlansin. P=4*a
+*/
+
+import java.util.Scanner
+
+fun main()
+{
+	val scanner : Scanner = Scanner(System.`in`)
+
+	print("Kvadrattin’ tarepin kiritin' a=")
+	var a = scanner.nextInt()
+    println()
+	var P = 4*a
+
+	print("Kvadrattin’ perimetri P=$P")
+}
